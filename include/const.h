162384a9b2bc4cdfaf4521aca24541b93b7959e3
jonathanhu237 <jonathanhu237@gmail.com>
2025-06-30 16:34:28 +0800

    fix: change several constants to double
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- include/const.h
+++ include/const.h
@@ -6,12 +6,12 @@
 
 namespace qalsh_chamfer {
 
-const float kEpsilon = 1e-6;
+const double kEpsilon = 1e-6;
 
 const std::string kDefaultParentDirectory = "data";
 const int kDefaultLeftBoundary = -128;  // For dataset generation
 const int kDefaultRightBoundary = 128;  // For dataset generation
-const float kDefaultErrorProbability = 1.0 / std::numbers::e_v<float>;
+const double kDefaultErrorProbability = 1.0 / std::numbers::e_v<double>;
 
 }  // namespace qalsh_chamfer
 
