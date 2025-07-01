f413a61a4aa82d451f3bfeb55cf641f92eef17c2
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-01 20:41:21 +0800

    feat: add DotProduct method to Utils class
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,6 +2,7 @@
 #define UTILS_H_
 
 #include <Eigen/Dense>
+#include <vector>
 
 namespace qalsh_chamfer {
 
@@ -10,6 +11,8 @@ class Utils {
     auto static CalculateChamfer(const std::vector<std::vector<double>>& from_set,
                                  const std::vector<std::vector<double>>& to_set, const std::string& from_set_name = "",
                                  const std::string& to_set_name = "", bool verbose = false) -> double;
+
+    auto static DotProduct(const std::vector<double>& vec1, const std::vector<double>& vec2) -> double;
 
    private:
     auto static ToEigenMatrix(const std::vector<std::vector<double>>& set)
--- src/utils.cc
+++ src/utils.cc
@@ -4,6 +4,11 @@
 #include <cstddef>
 #include <format>
 #include <iostream>
+#include <stdexcept>
+
+#include "Eigen/src/Core/Map.h"
+#include "Eigen/src/Core/Matrix.h"
+#include "Eigen/src/Core/util/Meta.h"
 
 namespace qalsh_chamfer {
 
@@ -29,6 +34,17 @@ auto Utils::CalculateChamfer(const std::vector<std::vector<double>>& from_set,
         std::cout << "\n";
     }
     return chamfer_distance;
+}
+
+auto Utils::DotProduct(const std::vector<double>& vec1, const std::vector<double>& vec2) -> double {
+    if (vec1.size() != vec2.size()) {
+        throw std::invalid_argument("Vectors must be of the same size for dot product.");
+    }
+
+    Eigen::Map<const Eigen::VectorXd> eigen_vec1(vec1.data(), static_cast<Eigen::Index>(vec1.size()));
+    Eigen::Map<const Eigen::VectorXd> eigen_vec2(vec2.data(), static_cast<Eigen::Index>(vec2.size()));
+
+    return eigen_vec1.dot(eigen_vec2);
 }
 
 auto Utils::ToEigenMatrix(const std::vector<std::vector<double>>& set)
