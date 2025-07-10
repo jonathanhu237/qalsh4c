e8ce3903a562cc36da56792e23a6196f229b0106
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-10 20:24:33 +0800

    feat: add a `--verbose` option to the main executable
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,15 @@
+#include <spdlog/common.h>
+#include <spdlog/spdlog.h>
+
 #include <CLI/CLI.hpp>
 #include <exception>
 #include <format>
 
 auto main(int argc, char** argv) -> int {
     CLI::App app{"Fast Chamfer Distance Approximation via Query-Aware Locality-Sensitive Hashing (QALSH)."};
+
+    app.add_flag_callback(
+        "-v,--verbose", []() { spdlog::set_level(spdlog::level::debug); }, "Enable verbose (debug) logging");
 
     try {
         CLI11_PARSE(app, argc, argv);
