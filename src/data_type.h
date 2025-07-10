e594a97b267fa1410929815dc110cccb2cfef456
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-10 21:14:39 +0800

    feat: introduce `data_type.h` for multi-type handling
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

---
+++ src/data_type.h
@@ -0,0 +1,35 @@
+#ifndef DATA_TYPE_H
+#define DATA_TYPE_H
+
+#include <cstdint>
+#include <map>
+#include <string>
+#include <string_view>
+
+namespace qalsh_chamfer {
+
+enum class DataType : uint8_t { kUnknown, kUint8, kInt, kDouble };
+
+constexpr auto to_string(DataType data_type) -> std::string_view {
+    switch (data_type) {
+        case DataType::kUnknown:
+            return "unknown";
+        case DataType::kUint8:
+            return "uint8";
+        case DataType::kInt:
+            return "int";
+        case DataType::kDouble:
+            return "double";
+    }
+}
+
+const std::map<std::string, DataType> data_type_map = {
+    {"unknown", DataType::kUnknown},
+    {"uint8", DataType::kUint8},
+    {"int", DataType::kInt},
+    {"double", DataType::kDouble},
+};
+
+}  // namespace qalsh_chamfer
+
+#endif
