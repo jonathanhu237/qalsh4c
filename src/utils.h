6cd57bcd8afc8ab7e01db6768acc053fff0010bc
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-11 15:04:59 +0800

    refactor: change to_string method to return std::string_view for better performance
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- src/utils.h
+++ src/utils.h
@@ -2,22 +2,25 @@
 #define UTILS_H_
 
 #include <cstdint>
-#include <string>
+#include <string_view>
 
 class Utils {
    public:
     template <typename T>
-    static auto to_string() -> std::string {
-        if constexpr (std::is_same_v<T, uint8_t>) {
-            return "uint8";
-        } else if constexpr (std::is_same_v<T, int>) {
-            return "int";
-        } else if constexpr (std::is_same_v<T, double>) {
-            return "double";
-        } else {
-            return "unknown";
-        }
-    }
+    static constexpr auto to_string() -> std::string_view;
 };
+
+template <typename T>
+constexpr auto Utils::to_string() -> std::string_view {
+    if constexpr (std::is_same_v<T, uint8_t>) {
+        return "uint8";
+    } else if constexpr (std::is_same_v<T, int>) {
+        return "int";
+    } else if constexpr (std::is_same_v<T, double>) {
+        return "double";
+    } else {
+        static_assert(sizeof(T) == 0, "Unsupported type for Utils::to_string");
+    }
+}
 
 #endif
