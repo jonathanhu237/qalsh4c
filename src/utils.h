c6e07f91b6d731e307e063d929b1382c3bcf6bdb
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-14 06:35:18 +0000

    feat: add WriteToBuffer and ReadFromBuffer methods to Utils class
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- src/utils.h
+++ src/utils.h
@@ -23,6 +23,12 @@ class Utils {
 
     template <typename T1, typename T2>
     static auto DotProduct(const std::vector<T1> &vec1, const std::vector<T2> &vec2) -> double;
+
+    template <typename T>
+    auto static WriteToBuffer(std::vector<char> &buffer, size_t &offset, const T &data) -> void;
+
+    template <typename T>
+    auto static ReadFromBuffer(const std::vector<char> &buffer, size_t &offset) -> T;
 };
 
 template <typename T>
@@ -68,6 +74,28 @@ auto Utils::DotProduct(const std::vector<T1> &vec1, const std::vector<T2> &vec2)
     Eigen::Map<const Eigen::Matrix<T2, Eigen::Dynamic, 1>> eigen_vec2(vec2.data(), vec2.size());
 
     return eigen_vec1.dot(eigen_vec2);
+}
+
+template <typename T>
+auto Utils::WriteToBuffer(std::vector<char> &buffer, size_t &offset, const T &data) -> void {
+    if (offset + sizeof(T) > buffer.size()) {
+        throw std::out_of_range("Not enough space in buffer to write.");
+    }
+    std::memcpy(&buffer[offset], &data, sizeof(T));
+    offset += sizeof(T);
+}
+
+template <typename T>
+auto Utils::ReadFromBuffer(const std::vector<char> &buffer, size_t &offset) -> T {
+    if (offset + sizeof(T) > buffer.size()) {
+        throw std::out_of_range("Not enough data in buffer to read.");
+    }
+
+    T data;
+    std::memcpy(&data, &buffer[offset], sizeof(T));
+    offset += sizeof(T);
+
+    return data;
 }
 
 #endif
