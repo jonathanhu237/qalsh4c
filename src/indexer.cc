39fe8e9191f7db0a8455f6e8d874d3df44d2572e
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-15 16:12:11 +0000

    refactor: integrate configuration printing into QalshIndexer constructor and remove PrintConfiguration method
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- src/indexer.cc
+++ src/indexer.cc
@@ -32,11 +32,31 @@ QalshIndexer::QalshIndexer(std::filesystem::path dataset_directory, QalshConfigu
     base_reader_ =
         PointSetReaderFactory::Create(in_memory_, dataset_metadata_.data_type, dataset_directory_ / "base.bin",
                                       dataset_metadata_.base_num_points, dataset_metadata_.num_dimensions);
+
+    // Print the configuration
+    spdlog::debug(
+        "The configuration is as follows:\n"
+        "    Dataset Directory: {}\n"
+        "    Number of points in base set: {}\n"
+        "    Number of points in query set: {}\n"
+        "    Number of Dimensions: {}\n"
+        "    Data Type: {}\n"
+        "    Approximation Ratio: {}\n"
+        "    Bucket Width: {}\n"
+        "    Beta: {}\n"
+        "    Error Probability: {}\n"
+        "    Number of Hash Tables: {}\n"
+        "    Collision Threshold: {}\n"
+        "    Page Size: {}\n"
+        "    In Memory: {}\n"
+        "-----------------------------------------------------",
+        dataset_directory_.string(), dataset_metadata_.base_num_points, dataset_metadata_.query_num_points,
+        dataset_metadata_.num_dimensions, dataset_metadata_.data_type, qalsh_config_.approximation_ratio,
+        qalsh_config_.bucket_width, qalsh_config_.beta, qalsh_config_.error_probability, qalsh_config_.num_hash_tables,
+        qalsh_config_.collision_threshold, qalsh_config_.page_size, in_memory_);
 }
 
 void QalshIndexer::BuildIndex() {
-    PrintConfiguration();
-
     // Create the index directory if it does not exist
     std::filesystem::path index_directory = dataset_directory_ / "qalsh_index";
     if (!std::filesystem::exists(index_directory)) {
@@ -93,28 +113,4 @@ void QalshIndexer::BuildIndex() {
     // Save the QALSH configuration
     spdlog::info("Saving QALSH configuration...");
     qalsh_config_.Save(index_directory / "config.toml");
-}
-
-void QalshIndexer::PrintConfiguration() const {
-    spdlog::debug(R"(The configuration is as follows:
----------- QALSH Indexer Configuration ----------
-Dataset Directory: {}
-Number of points in base set: {}
-Number of points in query set: {}
-Number of Dimensions: {}
-Data Type: {}
-Approximation Ratio: {}
-Bucket Width: {}
-Beta: {}
-Error Probability: {}
-Number of Hash Tables: {}
-Collision Threshold: {}
-Page Size: {}
-In Memory: {}
------------------------------------------------------)",
-                  dataset_directory_.string(), dataset_metadata_.base_num_points, dataset_metadata_.query_num_points,
-                  dataset_metadata_.num_dimensions, dataset_metadata_.data_type, qalsh_config_.approximation_ratio,
-                  qalsh_config_.bucket_width, qalsh_config_.beta, qalsh_config_.error_probability,
-                  qalsh_config_.num_hash_tables, qalsh_config_.collision_threshold, qalsh_config_.page_size,
-                  in_memory_);
 }
