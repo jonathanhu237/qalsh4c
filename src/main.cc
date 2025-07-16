eacd2de844d96b7554174c316a35e60268c8695e
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-16 07:20:12 +0000

    feat: add TerminatingSink to force program exit when spdlog::error is called
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,19 @@
+
+#include <spdlog/common.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 #include <CLI/CLI.hpp>
 #include <format>
 #include <memory>
+#include <mutex>
 
 #include "command.h"
 #include "constants.h"
 #include "dataset_generator.h"
 #include "estimator.h"
 #include "indexer.h"
+#include "sink.h"
 #include "types.h"
 
 int main(int argc, char** argv) {
@@ -17,8 +22,14 @@ int main(int argc, char** argv) {
     std::string log_level;
     app.add_option("-l,--log_level", log_level, "Set the logging level (default: info)")
         ->default_val("info")
-        ->check(CLI::IsMember({"debug", "info"}))
-        ->each([&](const std::string& level) { spdlog::set_level(spdlog::level::from_str(level)); });
+        ->check(CLI::IsMember({"debug", "info", "error"}))
+        ->each([&](const std::string& level) {
+            auto console_sink = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
+            auto terminating_sink = std::make_shared<TerminatingSink<std::mutex>>(console_sink);
+            auto logger = std::make_shared<spdlog::logger>("qalsh_chamfer", terminating_sink);
+            logger->set_level(spdlog::level::from_str(level));
+            spdlog::set_default_logger(logger);
+        });
 
     app.require_subcommand(1);
     std::unique_ptr<Command> command;
