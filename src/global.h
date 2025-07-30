f67720c9622ab694bb8fb55c1ec2b7c4aee721bd
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-30 12:34:59 +0800

    feat: add Global class with constants for mathematical operations
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

---
+++ src/global.h
@@ -0,0 +1,15 @@
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#include <cmath>
+#include <numbers>
+
+class Global {
+   public:
+    // Constants
+    static constexpr double kEpsilon = 1e-6;
+    static constexpr unsigned int kDefaultPageSize = 4096;
+    static constexpr double kDefaultErrorProbability = 1.0 / std::numbers::e_v<double>;
+};
+
+#endif
