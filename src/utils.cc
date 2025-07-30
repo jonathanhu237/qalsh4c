ea5887e3f7bffc25155066ee04a2d3dabbf438ac
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-30 14:08:53 +0800

    feat: add Utils class with L1Distance method for vector distance calculation
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

---
+++ src/utils.cc
@@ -0,0 +1,14 @@
+#include "utils.h"
+
+#include <Eigen/src/Core/util/Meta.h>
+
+double Utils::L1Distance(const Point &pt1, const Point &pt2) {
+    if (pt1.size() != pt2.size()) {
+        spdlog::error("Vectors must be of the same size");
+    }
+
+    Eigen::Map<const Eigen::VectorXf> v1(pt1.data(), static_cast<Eigen::Index>(pt1.size()));
+    Eigen::Map<const Eigen::VectorXf> v2(pt2.data(), static_cast<Eigen::Index>(pt2.size()));
+
+    return static_cast<double>((v1 - v2).lpNorm<1>());
+}
---
+++ src/utils.h
@@ -0,0 +1,16 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <spdlog/spdlog.h>
+#include <toml++/toml.h>
+
+#include <Eigen/Eigen>
+
+#include "point_set.h"
+
+class Utils {
+   public:
+    static double L1Distance(const Point &pt1, const Point &pt2);
+};
+
+#endif
