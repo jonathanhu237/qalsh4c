7cee80d40581c879f21fabc2a6702ff757967c03
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-31 16:34:24 +0800

    feat: implement GenerateDatasetCommand to create and save point sets with metadata
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- src/command.cc
+++ src/command.cc
@@ -1,12 +1,79 @@
 #include "command.h"
 
+#include <algorithm>
 #include <chrono>
+#include <cmath>
 #include <format>
 #include <iostream>
+#include <memory>
+#include <random>
 #include <ratio>
 
+#include "dataset_metadata.h"
 #include "estimator.h"
+#include "point_set.h"
 
+// --------------------------------------------------
+// GenerateDatasetCommand Implementation
+// --------------------------------------------------
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+GenerateDatasetCommand::GenerateDatasetCommand(DatasetMetadata dataset_metadata, double left_boundary,
+                                               double right_boundary, std::filesystem::path output_directory,
+                                               bool in_memory)
+    : dataset_metadata_(dataset_metadata),
+      left_boundary_(left_boundary),
+      right_boundary_(right_boundary),
+      output_directory_(std::move(output_directory)),
+      in_memory_(in_memory),
+      gen_(std::random_device{}()) {}
+
+void GenerateDatasetCommand::Execute() {
+    // Create the directory if it does not exist.
+    if (!std::filesystem::exists(output_directory_)) {
+        spdlog::info("Creating dataset directory: {}", output_directory_.string());
+        std::filesystem::create_directory(output_directory_);
+    }
+
+    // Generate the point sets.
+    spdlog::info("Generating point set A...");
+    GeneratePointSet(output_directory_ / "A.bin", dataset_metadata_.num_points_a);
+
+    spdlog::info("Generating point set B...");
+    GeneratePointSet(output_directory_ / "B.bin", dataset_metadata_.num_points_b);
+
+    // We must save the metadata first since it will be used by the estimator (update the chamfer distance later)
+    dataset_metadata_.Save(output_directory_ / "metadata.toml");
+
+    // Calculate the Chamfer distance between the base and query sets
+    spdlog::info("Calculating Chamfer distance between base and query sets...");
+    AnnEstimator ann_estimator(std::make_unique<LinearScanAnnSearcher>());
+    dataset_metadata_.chamfer_distance = ann_estimator.Estimate(output_directory_);
+
+    // Save the updated metadata
+    dataset_metadata_.Save(output_directory_ / "metadata.toml");
+}
+
+void GenerateDatasetCommand::GeneratePointSet(const std::string &point_set_file_path, unsigned int num_points) {
+    std::unique_ptr<PointSetWriter> pointset;
+    if (in_memory_) {
+        pointset = std::make_unique<InMemoryPointSetWriter>(point_set_file_path);
+    } else {
+        pointset = std::make_unique<DiskPointSetWriter>(point_set_file_path);
+    }
+
+    std::uniform_real_distribution<double> dist(left_boundary_, right_boundary_);
+    for (unsigned int i = 0; i < num_points; i++) {
+        Point point(dataset_metadata_.num_dimensions);
+        std::ranges::generate(point, [&]() { return dist(gen_); });
+        pointset->AddPoint(point);
+    }
+
+    pointset->Flush();
+}
+
+// --------------------------------------------------
+// EstimateCommand Implementation
+// --------------------------------------------------
 EstimateCommand::EstimateCommand(std::unique_ptr<Estimator> estimator, std::filesystem::path dataset_directory,
                                  bool in_memory)
     : estimator_(std::move(estimator)), dataset_directory_(std::move(dataset_directory)), in_memory_(in_memory) {}
--- src/command.h
+++ src/command.h
@@ -3,7 +3,9 @@
 
 #include <filesystem>
 #include <memory>
+#include <random>
 
+#include "dataset_metadata.h"
 #include "estimator.h"
 
 // NOLINTNEXTLINE(cppcoreguidelines-special-member-functions)
@@ -13,10 +15,27 @@ class Command {
     virtual void Execute() = 0;
 };
 
+class GenerateDatasetCommand : public Command {
+   public:
+    GenerateDatasetCommand(DatasetMetadata dataset_metadata, double left_boundary, double right_boundary,
+                           std::filesystem::path output_directory, bool in_memory);
+    void Execute() override;
+
+   private:
+    void GeneratePointSet(const std::string &point_set_file_path, unsigned int num_points);
+
+    DatasetMetadata dataset_metadata_;
+    double left_boundary_;
+    double right_boundary_;
+    std::filesystem::path output_directory_;
+    bool in_memory_;
+
+    std::mt19937 gen_;
+};
+
 class EstimateCommand : public Command {
    public:
-    explicit EstimateCommand(std::unique_ptr<Estimator> estimator, std::filesystem::path dataset_directory,
-                             bool in_memory);
+    EstimateCommand(std::unique_ptr<Estimator> estimator, std::filesystem::path dataset_directory, bool in_memory);
     void Execute() override;
 
    private:
