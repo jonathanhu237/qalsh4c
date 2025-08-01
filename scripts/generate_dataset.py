4451875c30ed4e2b476168f875d114ea8d53d88b
jonathanhu237 <jonathanhu237@gmail.com>
2025-08-01 16:15:22 +0800

    refactor: move chamfer distance calculation to utils and update imports
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- scripts/generate_dataset.py
+++ scripts/generate_dataset.py
@@ -4,8 +4,7 @@ import time
 from pathlib import Path
 
 import numpy as np
-from chamfer_distance import chamfer_distance
-from utils import create_metadata, save_binary_data, setup_logging
+from utils import chamfer_distance, create_metadata, save_binary_data, setup_logging
 
 
 def generate_point_set(
