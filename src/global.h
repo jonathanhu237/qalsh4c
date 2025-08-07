8f062c717e387bb9d8c7f6747d611b13fcc2d726
jonathanhu237 <jonathanhu237@gmail.com>
2025-08-07 13:41:22 +0800

    refactor(global): remove dataset generation constants and add kScanSize
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- src/global.h
+++ src/global.h
@@ -6,19 +6,12 @@
 
 class Global {
    public:
-    // Constants
     static constexpr double kEpsilon = 1e-6;
     static constexpr unsigned int kDefaultPageSize = 4096;
     static constexpr double kDefaultErrorProbability = 1.0 / std::numbers::e_v<double>;
     static constexpr double kDefaultApproximationRatio = 2.0;
     static constexpr unsigned int kNumCandidates = 100;
-
-    // For dataset generation
-    static constexpr unsigned int kDefaultNumPointsA = 1000;
-    static constexpr unsigned int kDefaultNumPointsB = 1000;
-    static constexpr unsigned int kDefaultNumDimensions = 256;
-    static constexpr double kDefaultLeftBoundary = -1024;
-    static constexpr double kDefaultRightBoundary = 1024;
+    static constexpr unsigned int kScanSize = 128;
 };
 
 #endif
