071fc58bfae6bdb61f8887797321955d591a2626
jonathanhu237 <jonathanhu237@gmail.com>
2025-08-07 15:04:10 +0800

    refactor(utils): remove redundant size checks in distance functions
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- src/utils.cc
+++ src/utils.cc
@@ -10,10 +10,6 @@
 #include "global.h"
 
 double Utils::L1Distance(const Point &pt1, const Point &pt2) {
-    if (pt1.size() != pt2.size()) {
-        spdlog::error("Vectors must be of the same size");
-    }
-
     Eigen::Map<const Eigen::VectorXd> v1(pt1.data(), static_cast<Eigen::Index>(pt1.size()));
     Eigen::Map<const Eigen::VectorXd> v2(pt2.data(), static_cast<Eigen::Index>(pt2.size()));
 
@@ -21,11 +17,6 @@ double Utils::L1Distance(const Point &pt1, const Point &pt2) {
 }
 
 double Utils::DotProduct(const Point &pt1, const Point &pt2) {
-    if (pt1.size() != pt2.size()) {
-        spdlog::error("Points must be of the same size for dot product. vec1.size(): {}, vec2.size(): {}", pt1.size(),
-                      pt2.size());
-    }
-
     Eigen::Map<const Eigen::VectorXd> v1(pt1.data(), static_cast<Eigen::Index>(pt1.size()));
     Eigen::Map<const Eigen::VectorXd> v2(pt2.data(), static_cast<Eigen::Index>(pt2.size()));
 
